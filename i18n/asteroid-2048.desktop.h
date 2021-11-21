02de8c1021304ab7e7e2c5a28094140d49930d95
MagneFire <IDaNLContact@gmail.com>
2021-11-21 14:29:08 +0100

    Port to CMake.

---
+++ i18n/asteroid-2048.desktop.h
@@ -0,0 +1,6 @@
+/* This dummy header is just used to generate a translatable Qt string that
+ * will be integrated to i18n/.ts files, translated on Weblate and then merged
+ * back with the .desktop.template file into a single .desktop file. */
+
+//% "2048"
+qtTrId("id-app-launcher-name");
